58bf1540a374746f63f50f58d9d830c54ba305a4
Ivanmsdp <lookzii525@gmail.com>
2024-11-23 02:34:05 -0600

    Actualizar ipgeolocation.py

--- ipgeolocation.py
+++ ipgeolocation.py
@@ -2,30 +2,28 @@
 # encoding: UTF-8
 
 """
-    This file is part of IPGeoLocation tool.
-    Copyright (C) 2015-2016 @maldevel
-    https://github.com/maldevel/IPGeoLocation
+    Herramienta IP_GeoLocation Tool.
     
-    IPGeoLocation - Retrieve IP Geolocation information 
-    Powered by http://ip-api.com
     
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    IP_Locacion - Recuperar Informacion De Geo_localizacion Ip
+    Desarrollado Por http://ip-api.com
+     
+    Este Programa Es Software Libre:Puedes      Redistribuirlo y/o Modificarlo. 
+    Bajo Los Términos De La Licencia Pública General GNU Publicada Por La Free Software Foundation.
+     Ya Sea La versión De La Licencia,O(A Su Elección) Cualquier Versión Posterior.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    Este Programa Se Distribuye Con La Esperanza De Que Sea Util.
+    Pero SIN NINGUNA GARANTÍA; Sin Siquiera La Garantía Implícita De COMERCIABILIDAD o IDONEIDAD PARA UN PROPÓSITO PARTICULAR.
+ Ver el Licencia pública general GNU para obtener más detalles.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    Debería Haber Recibido Una Copia De La Licencia Pública General GNU Junto Con Este Programa. 
+    Si No, Ver <http://www.gnu.org/licenses/>.
     
-    For more see the file 'LICENSE' for copying permission.
+    Para Obtener Más Información, Consulte El Archivo 'LICENCIA' Para Obtener Permiso De Copia. 
+
 """
 
-__author__  = 'maldevel'
+__author__  = 'Ivan-M-YT-TLM-SDP'
 
 
 import sys, os
@@ -49,47 +47,47 @@ def main():
         
     logger = Logger(args.nolog, args.verbose)
     
-    #single target or multiple targets 
+    #objetivo único o múltiples objetivos 
     if(args.target and args.tlist):
-        logger.PrintError("You can request Geolocation information either for a single target(-t) or a list of targets(-T). Not both!", args.nolog)
+        logger.PrintError("Puede solicitar información de Geolocalización ya sea para un solo objetivo (-t) o una lista de objetivos (-T). No ambos!", args.nolog)
         sys.exit(2)
         
-    #my ip address or single target
+    #mi dirección IP o objetivo único
     if(args.target and args.myip):
-        logger.PrintError("You can request Geolocation information either for a single target(-t) or your own IP address. Not both!", args.nolog)
+        logger.PrintError("Puede solicitar información de geolocalización ya sea para un único objetivo (-t) o su propia dirección IP. No ambos!", args.nolog)
         sys.exit(3)
         
-    #multiple targets or my ip address
+    #múltiples objetivos o mi dirección IP
     if(args.tlist and args.myip):
-        logger.PrintError("You can request Geolocation information either for a list of targets(-T) or your own IP address. Not both!", args.nolog)
+        logger.PrintError("Puede solicitar información de geolocalización ya sea para un único objetivo (-t) o su propia dirección IP. No ambos!", args.nolog)
         sys.exit(4)
     
-    #single target and google maps only allowed
+    #Solo se permiten objetivos únicos y Google Maps.
     if(args.tlist and args.g):
-        logger.PrintError("Google maps location is working only with single targets.", args.nolog)
+        logger.PrintError("La ubicación de Google Maps funciona solo con objetivos únicos.", args.nolog)
         sys.exit(5)
     
-    #specify user-agent or random
+    #Especificar usuario-agente o aleatorio
     if(args.uagent and args.ulist):
-        logger.PrintError("You can either specify a user-agent string or let IPGeolocation pick random user-agent strings for you from a file.", args.nolog)
+        logger.PrintError("Puede especificar una cadena de agente de usuario o dejar que IPGeolocation elija cadenas de agente de usuario aleatorias de un archivo.", args.nolog)
         sys.exit(6)
         
-    #specify proxy or random
+    #proxy aleatorio
     if(args.proxy and args.xlist):
-        logger.PrintError("You can either specify a proxy or let IPGeolocation pick random proxy connections for you from a file.", args.nolog)
+        logger.PrintError("Puede especificar un proxy o dejar que IPGeolocation elija conexiones de proxy aleatorias desde un archivo.", args.nolog)
         sys.exit(7)
         
         
-    #init lib
+    #biblioteca de inicio
     ipGeoLocRequest = IpGeoLocationLib(args.target, logger, args.noprint)
     
     print(banner)
     
-    #retrieve information
+    #Recuperar Información
     if not ipGeoLocRequest.GetInfo(args.uagent, args.tlist, 
                                      args.ulist, args.proxy, args.xlist,
                                      args.csv, args.xml, args.txt, args.g):
-        logger.PrintError("Retrieving IP Geolocation information failed.")
+        logger.PrintError("Error al recuperar la información de geolocalización de IP.")
         sys.exit(8)
 
 
